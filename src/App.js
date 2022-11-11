0d997297c13207940648fd053551df44933d614b
ridhorp <ridhogoodevil@gmail.com>
2022-11-11 14:23:48 +0700

    - save to local storage - install redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
@@ -6,6 +6,25 @@ import TodoList from "./components/TodoList";
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    saveLocalTodos()
+  }, [todos]);
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  // const getLocalTodos = () => {
+  //   if (localStorage.getItem("todos") === null) {
+  //     localStorage.setItem("todos", JSON.stringify([]));
+  //   } else {
+  //     let todoLocal = JSON.parse(localStorage.getItem("todos"));
+  //     console.log(todoLocal);
+  //     setTodos(todoLocal);
+  //   }
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
